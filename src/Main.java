18f4c5ed1419cd1e3100c58ba88b76e301806fa5
Mirek <kprocbtt@gmail.com>
2023-06-27 09:50:09 +0200

    DodanieDwochCyfr

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//Zadanie: Napisz program, który zwraca sumę cyfr danej liczby.
+//Podpowiedź: Możesz przekształcić liczbę na ciąg znaków, a następnie zsumować wartości poszczególnych cyfr.
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int liczba = 1345;
+        String ciagZnakow = String.valueOf(liczba);
+        int suma = 0;
+        for (int i = 0; i < ciagZnakow.length(); i++) {
+            char cyfraChar = ciagZnakow.charAt(i); // pobranie kolejnej cyfry jako znak
+            int cyfra = Character.getNumericValue(cyfraChar); // przekształcenie znaku na liczbę
+            suma += cyfra; // dodanie cyfry do sumy
+        }
+
+        System.out.println(suma);
+
+    }
+}
+
